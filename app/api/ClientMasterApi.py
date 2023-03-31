9b717eb6119e913feab9a6f9d550dd89679888a1
Pooja Kumari <pooja931550@gmail.com>
2023-03-31 15:13:36 +0530

    data

--- app/api/ClientMasterApi.py
+++ app/api/ClientMasterApi.py
@@ -9,15 +9,20 @@ from ..databaseService.ClientMasterService import Client
 
 
 class UserAuthAPI(APIView):
-    def post(self, req):
-        client_key = req.data.get('clientkey')
-        url = req.headers.get('url')
-        auth_response = Client(client_key,url)
-        if auth_response:
-            return Response(auth_response, status.HTTP_200_OK)
-        else:
-            return Response("Data not found")
-            
+    def post(self,req):
+        try:
+            client_key = req.data.get('clientkey')
+            url = req.headers.get('url')
+            auth_response = Client(client_key,url)
+            if auth_response:
+                return Response(auth_response, status.HTTP_200_OK)
+            else:
+                return Response("Data not found")
+        except ClientMaster.DoesNotExist:
+                return Response ({
+                    'status':202,
+                    "message": "Invalid client key "
+                    })        
 
 
 
--- app/databaseService/ClientMasterService.py
+++ app/databaseService/ClientMasterService.py
@@ -3,14 +3,11 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from ..databaseModels.clientMaster import ClientMaster
-# from django.core.exceptions import ObjectDoesNotExist
-import requests
 from ..serializers.CheckwhitelistedSerializer import ClientMasterSerializer
 
 
 
 def Client(client_key,url):
-    try:
         user_obj = ClientMaster.objects.get(clientkey=client_key)
         serializer = ClientMasterSerializer(user_obj).data
         if serializer['clientURL']!=url:
@@ -24,12 +21,7 @@ def Client(client_key,url):
                  'message': 'Your Status Invalid !!'       
             })   
         return serializer
-    except ClientMaster.DoesNotExist:
-            return ({
-                 'status':202,
-                 "message": "Invalid client key "
-                 })
-
+    
 
 
   
